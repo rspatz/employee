8a3d1cfa5e6aa8b9cbe9acabe8bbedc0d6db8e99
Robert <rspatz@live.com>
2024-11-18 20:30:21 -0600

    Complete Search function of app, not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      employees: [],
+      searchField: ''
+    };
+    console.log('constructor');
+  }
+
+  componendDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { employees: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render');
+
+    const filteredEmployees = this.state.employees.filter(employee => {
+      return employee.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search employees'
+          onChange={event => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+
+        {filteredEmployees.map(employee => {
+          return (
+            <div key={employee.id}>
+              <h1>{employee.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
